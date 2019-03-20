bb9a5d7c67ff25c5af9e13dff3d5dce9ae9d323c
timfaner <929397876@qq.com>
2019-03-20 21:48:46 +0800

    bug fix

--- tools/rsync_client_setup.sh
+++ tools/rsync_client_setup.sh
@@ -1,5 +1,5 @@
 read -s -p "输入你的rsync备份密码:" PASSWD
-echo $PASSWD > /etc/rsyncd.pwd
-chmod 600 /etc/rsyncd.pwd
+echo $PASSWD > /etc/rsync.pwd
+chmod 600 /etc/rsync.pwd
 #rsync服务端需启用873端口
 firewall-cmd --zone=public --add-port=873/tcp --permanent
