3400d51ec65791c42d54de31ea157ab8b8331b22
timfaner <929397876@qq.com>
2019-03-20 21:04:25 +0800

    bug fix

--- tools/rsync_server_setup.sh
+++ tools/rsync_server_setup.sh
@@ -1,5 +1,5 @@
 chmod 640 ../configs/rsyncd.conf
-cp ./configs/rsyncd.conf /etc
+cp ../configs/rsyncd.conf /etc
 read -s -p "输入你的rsync备份密码:" PASSWD
 echo "backup:"$PASSWD > /etc/rsyncd.pwd
 chmod 600 /etc/rsyncd.pwd
