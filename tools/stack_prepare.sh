79b05e2d68ecb0822e6b7cff066afaeeef50bd49
timfaner <929397876@qq.com>
2019-03-20 19:29:19 +0800

    修改

--- tools/stack_prepare.sh
+++ tools/stack_prepare.sh
@@ -4,7 +4,7 @@ echo "stack ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/stack
 sudo su - stack
 sudo yum -y update
 sudo yum -y install   centos-release-scl
-sudo yum -y install  rh-python36
+sudo yum -y install  rh-python36 git
 sudo yum -y install python-pip
 pip install -U pip
 
